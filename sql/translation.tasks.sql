602a4d0756254eb107e856012afe5ac96fa5e08f
nmalessa <norman.malessa@trivago.com>
2017-09-07 16:45:38 +0200

    sql sample data

---
+++ sql/translation.tasks.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM translations.tasks
+LIMIT 0, 1000
+
+-- Date: 2017-09-07 16:44
+*/
+INSERT INTO `tasks` (`task_id`,`languages`,`tasks_date`,`user_id`,`title`,`state`,`original_document_id`) VALUES ('XCNT-1833','MX,PL,RU,TR','2017-05-24 11:19:59','amarcione','Translation of the tHM PRO Playbook','Done',464905);
+INSERT INTO `tasks` (`task_id`,`languages`,`tasks_date`,`user_id`,`title`,`state`,`original_document_id`) VALUES ('XCNT-2199','BR,DE,ES,FR,GR,IT,MX,NL,PL,PT,RU,TR','2017-07-11 11:46:43','amarcione','Translation of Zendesk PRO template','In Progress',481118);
+INSERT INTO `tasks` (`task_id`,`languages`,`tasks_date`,`user_id`,`title`,`state`,`original_document_id`) VALUES ('XCNT-2665','ES,IT','2017-09-04 10:13:21','amarcione','Translation of event material (flyer, one-pagers, folder)','In Progress',498801);
---
+++ sql/translation.users.sql
@@ -0,0 +1,21 @@
+/*
+-- Query: SELECT * FROM translations.users
+LIMIT 0, 1000
+
+-- Date: 2017-09-07 16:43
+*/
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('acristovao','XCNT-1833','Adelina Cristovao','adelina.cristovao@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('acristovao','XCNT-2199','Adelina Cristovao','adelina.cristovao@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('acristovao','XCNT-2665','Adelina Cristovao','adelina.cristovao@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('agras','XCNT-2199','Angeline Gras','Angeline.Gras@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('amarcione','XCNT-1833','Andrew Marcione','Andrew.Marcione@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('amarcione','XCNT-2199','Andrew Marcione','Andrew.Marcione@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('amarcione','XCNT-2665','Andrew Marcione','Andrew.Marcione@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('ecaschera','XCNT-1833','Emanuela Caschera','Emanuela.Caschera@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('ecaschera','XCNT-2199','Emanuela Caschera','Emanuela.Caschera@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('fhuettenhoff','XCNT-1833','Fabian Huettenhoff','Fabian.Huettenhoff@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('kkaveh','XCNT-2199','Kiana Kaveh','Kiana.Kaveh@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('mcarletti','XCNT-2199','Michela Carletti','michela.carletti@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('pribeiro','XCNT-1833','Pedro Ribeiro','Pedro.Ribeiro@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('rmunoz','XCNT-2199','Ricardo Muñoz','Ricardo.Munoz@trivago.com');
+INSERT INTO `users` (`user_id`,`task_id`,`talent`,`email`) VALUES ('rmunoz','XCNT-2665','Ricardo Muñoz','Ricardo.Munoz@trivago.com');
